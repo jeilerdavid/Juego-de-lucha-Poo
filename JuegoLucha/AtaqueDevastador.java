0b3cae578328bf27073f85c3df520e4d7a0a24cb
jeilerdavid <jdavidmedina@estudiante.uniajc.edu.co>
2025-05-22 22:21:08 -0500

    Delete JuegoLucha/AtaqueDevastador.java

--- JuegoLucha/AtaqueDevastador.java
+++
@@ -1,7 +0,0 @@
-public class AtaqueDevastador implements HabilidadEspecial {
-    @Override
-    public void usar(Personaje usuario, Personaje objetivo) {
-        System.out.println(usuario.getNombre() + " usa Ataque Devastador!");
-        objetivo.recibirDanio(40);
-    }
-}
