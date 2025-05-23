756051982716b9deb6049ea9e5ec279a055d3a28
jeilerdavid <jdavidmedina@estudiante.uniajc.edu.co>
2025-05-22 22:21:29 -0500

    Delete JuegoLucha/AumentoDefensa.java

--- JuegoLucha/AumentoDefensa.java
+++
@@ -1,7 +0,0 @@
-public class AumentoDefensa implements HabilidadEspecial {
-    @Override
-    public void usar(Personaje usuario, Personaje objetivo) {
-        usuario.activarDefensa();
-        System.out.println(usuario.getNombre() + " activó aumento de defensa.");
-    }
-}
